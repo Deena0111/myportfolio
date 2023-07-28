efef830a64dbb7e20af94d3e51bb42bb194bdbe5
szigonytech <jeyasimhan@szigonytech.com>
2023-07-28 12:39:41 +0530

    create website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { SkillComponent } from './components/skill/skill.component';
+import { WorkComponent } from './components/work/work.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HeroComponent,
+    AboutComponent,
+    SkillComponent,
+    WorkComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
